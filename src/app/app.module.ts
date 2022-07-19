bd2d84c41f73591ed0d8808a8e96ed68fc8ffa4d
joao-soares-2000 <joao.159.soares@gmail.com>
2022-07-19 10:39:16 -0300

    two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
@@ -11,6 +12,7 @@ import { EmiterComponent } from './components/emiter/emiter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { ChangeNumberBComponent } from './components/change-number-b/change-number-b.component';
+import { TwoWayBindingsComponent } from './components/two-way-bindings/two-way-bindings.component';
 
 @NgModule({
   declarations: [
@@ -23,10 +25,11 @@ import { ChangeNumberBComponent } from './components/change-number-b/change-numb
     EmiterComponent,
     ChangeNumberComponent,
     ListRenderComponent,
-    ChangeNumberBComponent
+    ChangeNumberBComponent,
+    TwoWayBindingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/two-way-bindings/two-way-bindings.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-bindings',
+  templateUrl: './two-way-bindings.component.html',
+  styleUrls: ['./two-way-bindings.component.css']
+})
+export class TwoWayBindingsComponent implements OnInit {
+  name: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
